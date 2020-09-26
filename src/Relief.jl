ed46a3987805ae62a21ed8fa555a0901678b5bd8
Jernej Vivod <je.vivod@gmail.com>
2020-09-26 12:23:51 +0200

    Add implementations and tests

---
+++ src/Relief.jl
@@ -0,0 +1,45 @@
+module Relief
+
+using StatsBase
+using Statistics
+using LinearAlgebra
+using Printf
+
+export 
+    relief,
+    relieff,
+    reliefseq,
+    reliefmss,
+    surfstar,
+    surf,
+    multisurfstar,
+    multisurf,
+    swrfstar,
+    boostedsurf,
+    iterative_relief,
+    irelief,
+    ecrelief,
+    vlsrelief,
+    turf
+
+# source files
+
+include("./utils/square_to_vec.jl")
+
+include("relief.jl")
+include("relieff.jl")
+include("reliefseq.jl")
+include("reliefmss.jl")
+include("surfstar.jl")
+include("surf.jl")
+include("multisurfstar.jl")
+include("multisurf.jl")
+include("swrfstar.jl")
+include("boostedsurf.jl")
+include("iterative_relief.jl")
+include("irelief.jl")
+include("ecrelieff.jl")
+include("vlsrelief.jl")
+include("turf.jl")
+
+end
---
+++ test/boostedsurf_test.jl
@@ -0,0 +1,39 @@
+using Test
+using Relief
+
+
+# Test functionality with continuous features.
+@testset "BoostedSURF - Continuous Features" begin
+    data = rand(500, 5)
+    for idx1 = 1:size(data, 2) - 1
+        for idx2 = idx1+1:size(data, 2)
+            target = Int64.(data[:, idx1] .> data[:, idx2])
+            weights = Relief.boostedsurf(data, target, f_type="continuous")
+            @test all(weights[idx1] .>= weights[(1:end .!= idx1) .& (1:end .!= idx2)])
+            @test all(weights[idx2] .>= weights[(1:end .!= idx1) .& (1:end .!= idx2)])
+        end
+    end
+end
+
+
+# Test functionality with discrete features.
+@testset "BoostedSURF - Discrete Features" begin
+    data = rand([0, 1, 2, 3], 500, 5)
+    for idx1 = 1:size(data, 2) - 1
+        for idx2 = idx1+1:size(data, 2)
+            target = Int64.(data[:, idx1] .> data[:, idx2])
+            weights = Relief.boostedsurf(data, target, f_type="discrete")
+            @test all(weights[idx1] .>= weights[(1:end .!= idx1) .& (1:end .!= idx2)])
+            @test all(weights[idx2] .>= weights[(1:end .!= idx1) .& (1:end .!= idx2)])
+        end
+    end
+end
+
+
+# Test exceptions.
+@testset "BoostedSURF - Exceptions" begin
+    data = rand([0, 1, 2, 3], 500, 5)
+    target = Int64.(data[:, 1] .> data[:, 2])
+    @test_throws DomainError Relief.boostedsurf(data, target, f_type="something_else")
+end
+
